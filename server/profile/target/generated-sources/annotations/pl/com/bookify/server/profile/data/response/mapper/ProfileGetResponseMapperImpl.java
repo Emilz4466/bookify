effd27f7ab7e2a8dfed046ee049b3749c510d660
Emil Zyberski <emil.z4466@gmail.com>
2024-12-02 11:51:11 +0100

    1.0.1_profile_microservice basic crud for profile with simple filter findAll fixes

---
+++ server/profile/target/generated-sources/annotations/pl/com/bookify/server/profile/data/response/mapper/ProfileGetResponseMapperImpl.java
@@ -0,0 +1,38 @@
+package pl.com.bookify.server.profile.data.response.mapper;
+
+import javax.annotation.processing.Generated;
+import pl.com.bookify.server.profile.Profile;
+import pl.com.bookify.server.profile.data.response.ProfileGetResponse;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-29T13:39:37+0100",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.13 (Eclipse Adoptium)"
+)
+public class ProfileGetResponseMapperImpl implements ProfileGetResponseMapper {
+
+    @Override
+    public ProfileGetResponse entityToResponse(Profile profile) {
+        if ( profile == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String firstName = null;
+        String email = null;
+        String password = null;
+        String role = null;
+        Boolean isAuthor = null;
+
+        id = profile.getId();
+        firstName = profile.getFirstName();
+        email = profile.getEmail();
+        password = profile.getPassword();
+        role = profile.getRole();
+        isAuthor = profile.getIsAuthor();
+
+        ProfileGetResponse profileGetResponse = new ProfileGetResponse( id, firstName, email, password, role, isAuthor );
+
+        return profileGetResponse;
+    }
+}
---
+++ server/profile/target/generated-sources/annotations/pl/com/bookify/server/profile/data/response/mapper/ProfilePostResponseMapperImpl.java
@@ -0,0 +1,38 @@
+package pl.com.bookify.server.profile.data.response.mapper;
+
+import javax.annotation.processing.Generated;
+import pl.com.bookify.server.profile.Profile;
+import pl.com.bookify.server.profile.data.response.ProfilePostResponse;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-11-29T13:39:36+0100",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.13 (Eclipse Adoptium)"
+)
+public class ProfilePostResponseMapperImpl implements ProfilePostResponseMapper {
+
+    @Override
+    public ProfilePostResponse entityToResponse(Profile profile) {
+        if ( profile == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String firstName = null;
+        String email = null;
+        String password = null;
+        String role = null;
+        Boolean isAuthor = null;
+
+        id = profile.getId();
+        firstName = profile.getFirstName();
+        email = profile.getEmail();
+        password = profile.getPassword();
+        role = profile.getRole();
+        isAuthor = profile.getIsAuthor();
+
+        ProfilePostResponse profilePostResponse = new ProfilePostResponse( id, firstName, email, password, role, isAuthor );
+
+        return profilePostResponse;
+    }
+}
